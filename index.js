20aa741e9394e4873e82ee8039ec91665edc96ad
shuaibird <shuaibird@icloud.com>
2019-01-02 15:13:30 +0800

    deduplicate the coordinates data; add a few text fields for the GA marker label

--- index.js
+++ index.js
@@ -18,18 +18,34 @@ trace({
   host,
   apiKey: process.env.IPAPI_API_KEY,
   cb(data) {
-    const coordinates = JSON.stringify(data.map(({ latitude, longitude }) => (
-      {
-        lat: latitude,
-        lng: longitude,
-      }
-    )))
+    const coordinates = data
+      .map(({ latitude, longitude, city, continent_name, country_name }) => (
+        {
+          lat: latitude,
+          lng: longitude,
+          city,
+          country: country_name,
+          continent: continent_name,
+        }
+      ))
+      .reduce((acc, cur) => {
+        const existed = acc.some(({ lat, lng }) => cur.lat === lat && cur.lng === lng)
+        if (existed) {
+          return acc
+        } else {
+          return [
+            ...acc,
+            cur,
+          ]
+        }
+      }, [])
+
     const { GOOGLE_API_KEY: apiKey } = process.env
 
     fs.readFile(templateFile, (err, template) => {
       const output = template
         .toString()
-        .replace('{{COORDINATES}}', coordinates)
+        .replace('{{COORDINATES}}', JSON.stringify(coordinates))
         .replace('{{GOOGLE_API_KEY}}', apiKey)
 
       fs.writeFile(outputFile, output, () => {
